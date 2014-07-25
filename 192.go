4c6624b8aced939a38c1db0f3d442ac671ed2776
Sam Boyer <tech@samboyer.org>
2014-07-25 14:20:40 -0400

    Add problem 192 solution

---
+++ 192.go
@@ -0,0 +1,28 @@
+package main
+
+func init() {
+	var winners int
+
+	answers[192] = func() {
+		var walker func(depth, l, a int)
+
+		walker = func(depth, l, a int) {
+			if depth > 30 {
+				winners++
+				return
+			}
+
+			walker(depth+1, l, 0) // represents an O
+			if a < 2 {
+				walker(depth+1, l, a+1) // represents an A
+			}
+			if l != 1 {
+				walker(depth+1, 1, 0) // represents an L
+			}
+		}
+
+		walker(1, 0, 0)
+		fml(winners)
+	}
+
+}
